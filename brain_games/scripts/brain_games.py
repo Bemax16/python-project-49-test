f5eaadbf0c3f78e93e984a0ce0618fbb0ecab6af
Daniil Verbitskiy <danilverbitskiy@gmail.com>
2024-08-02 20:16:51 +0300

    add welcome_user()

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,14 @@
+from ..cli import welcome_user
+
+
 def greeting():
     print('Welcome to the Brain Games!')
+    welcome_user()
+
 
 def main():
-    greeting() 
+    greeting()
+
 
 if __name__ == '__main__':
     main()
