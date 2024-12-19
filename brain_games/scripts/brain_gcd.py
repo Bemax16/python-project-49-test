b3f4831ef7cc092ffdb5a6f9ede541b20a20069e
Даниил Вербицкий <bemax@MacBook-Pro-Daniil.local>
2024-12-19 16:28:57 +0300

    add brain_gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,32 @@
+import random
+import math
+
+def play_gcd_game():
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+    print("Find the greatest common divisor of given numbers.")
+
+    rounds_to_win = 3
+    correct_answers = 0
+
+    while correct_answers < rounds_to_win:
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        print(f"Question: {num1} {num2}")
+        answer = input("Your answer: ").strip()
+
+        correct_answer = math.gcd(num1, num2)
+
+        if answer == str(correct_answer):
+            print("Correct!")
+            correct_answers += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
+if __name__ == "__main__":
+    play_gcd_game()
