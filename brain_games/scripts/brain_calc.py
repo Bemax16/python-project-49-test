46d23583177e6ce1603bf6aecc1ffce1be0b191d
Даниил Вербицкий <bemax@MacBook-Pro-Daniil.local>
2025-03-15 20:44:44 +0300

    fix

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,39 +1,21 @@
 import random
-
-from brain_games.scripts import interface
-
-
-def calculate_expression(num1, num2, operator):
-    if operator == "+":
-        return num1 + num2
-    elif operator == "-":
-        return num1 - num2
-    elif operator == "*":
-        return num1 * num2
+from brain_games.engine import run_game
 
 
-def play_calc_game():
-    name = interface.start_game()
-    print('What is the result of the expression?')
-    rounds_to_win = 3
-    correct_answers = 0
+def game_logic():
+    num1 = random.randint(1, 50)
+    num2 = random.randint(1, 50)
+    operator = random.choice(["+", "-", "*"])
 
-    while correct_answers < rounds_to_win:
-        num1 = random.randint(1, 50)
-        num2 = random.randint(1, 50)
-        operator = random.choice(["+", "-", "*"])
-        print(f"Question: {num1} {operator} {num2}")
-        answer = int(input("Your answer: "))
+    question = f"{num1} {operator} {num2}"
+    correct_answer = eval(question)
 
-        correct_answer = calculate_expression(num1, num2, operator)
-
-        correct_answers += interface.check_answer(answer, correct_answer, name)
-    print(f"Congratulations, {name}!")
+    return question, correct_answer
 
 
 def main():
-    play_calc_game()
+    run_game(game_logic, "What is the result of the expression?")
 
 
 if __name__ == "__main__":
-    play_calc_game()
+    main()
